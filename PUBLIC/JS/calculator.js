0a85b4a5b3b4e89980a89e5fea48e5b11edffb63
Raiyan Zarif <razash139@gmail.com>
2020-02-07 21:52:58 +0200

    polish

--- PUBLIC/JS/calculator.js
+++ PUBLIC/JS/calculator.js
@@ -1,3 +1,5 @@
+//Variables
+
 const previous = document.querySelector(".previous")
 const current = document.querySelector(".current")
 
@@ -12,13 +14,23 @@ const answerBtn = document.querySelector(".answer")
 let inputArr = []
 let currentVal = ""
 let previousVal = ""
-current.innerHTML = currentVal
-previous.innerHTML = previousVal
 let answer;
+let equalsPressed = false
+
+current.innerHTML = "0"
+previous.innerHTML = previousVal
+
+//Functions
 
 const append = (num) => {
-    currentVal += num
+    if (equalsPressed) {
+        allclear()
+    }
+
+    currentVal += num.toString()
     current.innerHTML = currentVal
+
+    equalsPressed = false
 }
 
 const operation = (oper) => {
@@ -51,46 +63,77 @@ const operation = (oper) => {
 
     current.innerHTML = currentVal
     previous.innerHTML = previousVal
+
+    equalsPressed = false
 }
 
 const allclear = () => {
     inputArr = []
     currentVal = ""
     previousVal = ""
-    current.innerHTML = currentVal
+    answer = undefined
+    current.innerHTML = "0"
     previous.innerHTML = previousVal
 }
 
 const backspace = () => {
+    if (equalsPressed) {
+        return
+    }
+
     currentVal = currentVal.slice(0, -1)
+
+    if (currentVal === "") {
+        current.innerHTML = "0"
+        return
+    }
+
     current.innerHTML = currentVal
 }
 
 const appendDecimal = () => {
+    if (equalsPressed) {
+        allclear()
+    }
+
     if (!currentVal.includes(".")) {
         currentVal += "."
         current.innerHTML = currentVal
     }
+
+    equalsPressed = false
 }
 
 const compute = () => {
+    if (currentVal === "") {
+        return
+    }
+
     inputArr.push(currentVal)
     previousVal += currentVal
+
     let arrStr = inputArr.join("")
     answer = eval(arrStr)
     currentVal = ""
     current.innerHTML = answer
     previous.innerHTML = previousVal
+    equalsPressed = true
 
 }
 
 const shift = () => {
+    if (!answer) {
+        return
+    }
+
     currentVal = answer
     previousVal = ""
     inputArr = []
     current.innerHTML = currentVal
     previous.innerHTML = previousVal
 }
+
+//Event Listeners
 
 numbersBtns.forEach(btn => {
     btn.addEventListener("click", () => {
